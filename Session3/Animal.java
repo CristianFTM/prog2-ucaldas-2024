533310ee500b7d5ecebb98cfabc4c9eaca959301
CristianFTM <cristian.170920621@ucaldas.edu.co>
2024-03-01 13:23:17 -0500

    Create de Animal class and method

---
+++ Session3/Animal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Animal {
+    Scanner sc = new Scanner(System.in);
+
+    public String name_animal = "";
+    public String type_animal = "";
+    public String breed_animal = "";
+
+    public void inputDataAnimal() {
+        System.out.println("Ingrese el numero del animal");
+        name_animal = sc.nextLine();
+
+        System.out.println("Ingrese el tipo del animal ej: gato, perro... ");
+        type_animal = sc.nextLine();
+
+        System.out.println("ingresa raza del animal");
+        breed_animal = sc.nextLine();
+
+    }
+
+}
