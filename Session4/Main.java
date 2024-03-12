5fa8b525ae073e9d59d39de78d0054292395189c
CristianFTM <cristian.170920621@ucaldas.edu.co>
2024-03-12 14:32:40 -0500

    End of session4

---
+++ Session4/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    static Exercise1 instance_exercise1 = new Exercise1();
+    static Exercise2 instance_exercise2 = new Exercise2();
+    static Exercise3 instance_exercise3 = new Exercise3();
+    static Agenda instance_agenda = new Agenda();
+
+    public static void main(String[] args) {
+        // System.out.println(instance_exercise1.sumaArrayNumeros());
+        // System.out.println(instance_exercise2.addArrayListCadenas());
+        // System.out.println(instance_exercise2.removeArrayListCadenas());
+        // System.out.println("Tamaño Arraylist " + instance_exercise2.getSize());
+        // System.out.println(instance_exercise3.addValues());
+        // System.out.println(instance_exercise3.searchValue());
+        instance_agenda.inputData();
+
+    }
+}
